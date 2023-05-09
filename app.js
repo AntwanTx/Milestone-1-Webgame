f3a21c0c1fc5f878d5e66d6c485e505f4aff9cf1
Anthony Munoz <amunoz021@gmail.com>
2023-05-08 21:33:55 -0500

    Added new nav bar and footer with directions with some css

---
+++ app.js
@@ -0,0 +1,33 @@
+
+// function changeDirection(e) {
+//     if (e.code == "ArrowUp" && speedY != 1) {
+//         // If up arrow key pressed with this condition...
+//         // snake will not move in the opposite direction
+//         speedX = 0;
+//         speedY = -1;
+//     }
+//     else if (e.code == "ArrowDown" && speedY != -1) {
+//         //If down arrow key pressed
+//         speedX = 0;
+//         speedY = 1;
+//     }
+//     else if (e.code == "ArrowLeft" && speedX != 1) {
+//         //If left arrow key pressed
+//         speedX = -1;
+//         speedY = 0;
+//     }
+//     else if (e.code == "ArrowRight" && speedX != -1) {
+//         //If Right arrow key pressed
+//         speedX = 1;
+//         speedY = 0;
+//     }
+// }
+ 
+// // Randomly place food
+// function placeFood() {
+ 
+//     // in x coordinates.
+//     foodX = Math.floor(Math.random() * total_col) * blockSize;
+     
+//     //in y coordinates.
+//     foodY = Math.floor(Math.random() * total_row) * blockSize;}
