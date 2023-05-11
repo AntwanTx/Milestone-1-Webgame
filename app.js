006b7eaaaf37e931d5eeb7f876401f6ad709c497
Anthony Munoz <amunoz021@gmail.com>
2023-05-10 23:44:29 -0500

    Snake body grows when food it eaten

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const gameBoard = document.querySelector('#snakepit');
-const scoreBoard = document.querySelector('score');
+const scoreBoard = document.querySelector('#score');
 const gameStart = document.getElementsByClassName('start');
 
 let anniePosition = [{ x:11, y: 11 }];
@@ -8,12 +8,9 @@ let speed = 10;
 let score = 0;
 let timeClock = 0;
 let food = { x:20, y:20 };
-/**
- * Runs the main game loop using window.requestAnimationFrame. If the time since the last
+/* Runs the main game loop using window.requestAnimationFrame. If the time since the last
  * game loop is less than 1 / speed in seconds, return early. Otherwise, update the timeClock
- * to the current time and call the gamePlay function.
- *
- */
+ * to the current time and call the gamePlay function.*/
 function main(gameStart) {
     window.requestAnimationFrame(main);
     if ((gameStart - timeClock) / 1000 < 1 / speed) {
@@ -22,9 +19,7 @@ function main(gameStart) {
     timeClock = gameStart;
     gamePlay();
 }
-/**
- * Updates the display with the remaining time
-  */
+/*Updates the display with the remaining time*/
 function timeLeft(duration, display){
 
     let timer = duration, 
@@ -47,19 +42,14 @@ function timeLeft(duration, display){
         }
     },1000)
 }
-/**
- * Starts a timer for one minute and displays the remaining time on the screen
- */
+/*Starts a timer for one minute and displays the remaining time on the screen*/
 function countDown() {
     let oneMinute = 60; // the duration of the timer in seconds
     let display = document.querySelector("#time-left"); // the element where the remaining time is displayed
     timeLeft(oneMinute, display); // start the timer
 }
 countDown()
-/*
- * Moves Annie's position on the game board based on the arrow direction.
- * Changes the arrow direction based on the key pressed.
- */
+/*Moves Annie's position on the game board based on the arrow direction.Changes the arrow direction based on the key pressed.*/
 function changeDirection(event) {
     switch (event.key) {
         case "ArrowUp":
@@ -80,6 +70,7 @@ document.addEventListener("keydown", changeDirection);
  window.requestAnimationFrame(main);
 
  /* Updates the game board with Annie's new position and the food element.
+ * Checks for collisions with food and increments the score if a collision occurs.
  */
 function gamePlay(){
     for (let i = anniePosition.length - 2; i >= 0; i--) {
@@ -113,34 +104,40 @@ function gamePlay(){
     foodElement.style.gridColumnStart = food.x;
     foodElement.classList.add("food");
     gameBoard.appendChild(foodElement);
-}
-/**
- * This function updates the game play with each move of the snake, 
- * and checks if the snake has collided with the food.
-    // Check if the snake has collided with the food */
+
+    // Check if the snake has collided with the food
     if (anniePosition[0].y === food.y && anniePosition[0].x === food.x) {
-      // Increase the score by 1 and update the score board
-      score += 1;
-      scoreBoard.innerHTML = " " + score;
-  
-      // Add a new segment to the front of the snake
-      anniePosition.unshift({
-        x: anniePosition[0].x + arrowDirection.x,
-        y: anniePosition[0].y + arrowDirection.y,
-      });
-  
-      // Generate a new food location
-      let a = 2;
-      let b = 23;
-      food = {
-        x: Math.round(a + (b - a) * Math.random()),
-        y: Math.round(a + (b - a) * Math.random()),
-      };
-  
-      // If the score is 10, display a message and reload the page
-      if(score === 10 ){
-        alert("ANNIE IS FULL...WAY TO GO!!!");
-        location.reload();
-      }
+        // Increase the score by 1 and update the score board
+        score += 1;
+        scoreBoard.innerHTML = " " + score;
+
+        // Add a new segment to the front of the snake
+        anniePosition.unshift({
+            x: anniePosition[0].x + arrowDirection.x,
+            y: anniePosition[0].y + arrowDirection.y,
+        });
+
+        // Generate a new food location
+        let a = 2;
+        let b = 23;
+        food = {
+            x: Math.round(a + (b - a) * Math.random()),
+            y: Math.round(a + (b - a) * Math.random()),
+        };
+
+        // If the score is 10, display a message and reload the page
+        if(score === 10 ){
+            alert("ANNIE IS FULL...WAY TO GO!!!");
+            location.reload();
+        }
     }
-    
+
+    // Check for collisions
+    if (annieSelfDestruct(anniePosition)) {
+        arrowDirection = { x: 0, y: 0 }; // Reset arrow direction.
+        alert("GAME OVER!!"); // Display game over message.
+        location.reload(); // Reload the page.
+        anniePosition = [{ x: 11, y: 11}]; // Reset Annie's position.
+        score = 0; // Reset the score.
+    }
+}
